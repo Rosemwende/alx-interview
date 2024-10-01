e87a2d9b4918bd141413ca74d3cdbbb82095b493
Rosemwende <rosemuema82@gmail.com>
2024-10-01 10:36:03 +0000

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,9 @@
 def pascal_triangle(n):
     triangle = []
-    
+
     if n <= 0:
         return triangle
-    
+
     for i in range(n):
         row = [1]
         if i > 0:
@@ -11,5 +11,5 @@ def pascal_triangle(n):
                 row.append(triangle[i-1][j-1] + triangle[i-1][j])
             row.append(1)
         triangle.append(row)
-    
+
     return triangle
