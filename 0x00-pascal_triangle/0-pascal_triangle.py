f75b513ee9e5d88ab5003327677e22f09c316b32
Rosemwende <rosemuema82@gmail.com>
2024-10-01 10:56:19 +0000

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,23 @@
 def pascal_triangle(n):
-    triangle = []
+"""
+Generates pascal's Triangle of size n.
 
-    if n <= 0:
-        return triangle
+Args:
+	n (int): The number of rows in the triangle.
+Returns:
+	List[List[int]]: A list of lists of integers representing pascal's Triangle.
+"""
+if n <= 0:
+        return []
 
-    for i in range(n):
+    triangle = [[1]]
+    
+    for i in range(1, n):
+        prev_row = triangle[-1]
         row = [1]
-        if i > 0:
-            for j in range(1, i):
-                row.append(triangle[i-1][j-1] + triangle[i-1][j])
-            row.append(1)
+        for j in range(1, i):
+            row.append(prev_row[j-1] + prev_row[j])
+        row.append(1)
         triangle.append(row)
 
     return triangle
