a8bbd2da175c733a75d9209f98dad91005a76b95
Rosemwende <rosemuema82@gmail.com>
2024-10-17 18:58:55 +0000

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """
-main file for testing
+Minimum Operations to achieve n 'H' characters using Copy All and Paste operations
 """
-minOperations = __import__('0-minoperations').minOperations
+def minOperations(n)
+if n <= 1:
+        return 0
 
-n = 4
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+    ops = 0
+    divisor = 2
 
-n = 12
-print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+    while n > 1:
+        while n % divisor == 0:
+            ops += divisor
+            n = n // divisor
+        divisor += 1
+
+    return ops
