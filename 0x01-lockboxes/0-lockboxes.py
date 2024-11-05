a33e791f7f99a5184e9ad2fd3f7b133585cfbf13
Rosemwende <rosemuema82@gmail.com>
2024-11-05 10:35:50 +0000

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,15 +9,15 @@ def canUnlockAll(boxes):
     Returns:
     bool: True if all boxes can be opened, False otherwise.
     """
-    if not isinstance(boxes, list):
+    if not isinstance(boxes, list) or not boxes:
         return False
 
     unlocked_boxes = set([0])
+    stack = list(boxes[0])
 
     while stack:
         key = stack.pop()
         if key < len(boxes) and key not in unlocked_boxes:
             unlocked_boxes.add(key)
             stack.extend(boxes[key])
-
-    return len(unlocked_boxes) == len(boxes)
+     return len(unlocked_boxes) == len(boxes)
