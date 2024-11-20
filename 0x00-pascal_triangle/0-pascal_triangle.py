1a15f0ff23c47784988a445fab8f487cbd5ae14c
Rosemwende <rosemuema82@gmail.com>
2024-11-20 12:55:54 +0000

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,14 +11,14 @@ Returns:
 """
 
 def pascal_triangle(n):
-"""
+    """
     Generate Pascal's Triangle of size n
 
     Args:
         n (int): The number of rows of the triangle
 
     Returns:
-       A list of lists of integers representing Pascal's Triangle
+        A list of lists of integers representing Pascal's Triangle
     """
     if n <= 0:
         return []
