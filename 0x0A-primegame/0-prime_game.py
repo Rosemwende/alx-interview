1230ec5bf3d483cc6a2d23c8a6484c6d9f033b65
rosemwende <rosemuema82@gmail.com>
2024-12-11 19:41:08 +0000

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,30 +2,34 @@
 """Prime Game"""
 
 def isWinner(x, nums):
-    """Determines who wins the most rounds based on the prime game rules"""
+    """Determines who wins the most rounds based on the prime game rules."""
     if x <= 0 or not nums:
         return None
 
     def sieve_of_eratosthenes(limit):
-        """Generates a list of prime numbers up to a given limit"""
+        """Generates a list of prime numbers up to a given limit."""
         sieve = [True] * (limit + 1)
         sieve[0] = sieve[1] = False
         for i in range(2, int(limit ** 0.5) + 1):
             if sieve[i]:
                 for j in range(i * i, limit + 1, i):
                     sieve[j] = False
-        return [i for i in range(limit + 1) if sieve[i]]
+        return sieve
 
     max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
+    sieve = sieve_of_eratosthenes(max_n)
+
+    prime_counts = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_counts[i] = prime_counts[i - 1] + (1 if sieve[i] else 0)
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        primes_in_range = [p for p in primes if p <= n]
+        primes_count = prime_counts[n]
 
-        if len(primes_in_range) % 2 == 1:
+        if primes_count % 2 == 1:
             maria_wins += 1
         else:
             ben_wins += 1
