7462b3d34df243b3a82d5a365dfe4ce17543734e
rosemwende <rosemuema82@gmail.com>
2024-12-20 17:47:18 +0000

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,6 +3,7 @@
 determine the fewest number of coins needed to meet a given amount total
 """
 
+
 def makeChange(coins, total):
     """
     Determines the fewest number of coins needed to meet a given total
